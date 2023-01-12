4fff8228da82a375235a723672e251830c973ed7
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-01-12 16:51:04 +0700

    correcting spelling

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,7 +7,9 @@ export const projects: ProjectType[] = [
     slug: "bayarplanner",
     description: [
       "BayarPlanner is a website that schedules and records daily financial transactions, also calculates data to create simple invoices.",
-      "Created using React JS, makes it easier to work with a lot of date related libraries, such as React DatePicker and DayJS",
+
+      "Created using React JS, makes it easier to work with a lot of date-related libraries, such as React DatePicker and DayJS",
+
       "Even so, the main calendar design is costumed made instead of using a ready-use library, so it can have more flexibility to handle different functions accordingly to activities that may be different each day.",
     ],
     mainProgram: "React.js",
@@ -20,9 +22,11 @@ export const projects: ProjectType[] = [
     title: "SudahNonton",
     slug: "sudahnonton",
     description: [
-      "SudahNonton is a movie and TV Shows database website that also serves as REST API provider that can be consume by other website or application.",
-      "The design is based on the Netflix website design using real data for The Movie DB and I chose to use Laravel so it can be both be used as Front end as well as Back End at the same time",
-      "I watched and I like all of the movies and TV shows that are shown on this website, so people will be able to watch it if they need a recommendation for movie night",
+      "SudahNonton is a movie and TV Shows database website that also serves as REST API provider that can be consumed by other websites or applications.",
+
+      "The design is based on the Netflix website design using real data for The Movie DB and I chose to use Laravel so it can be used as Front end as well as Back End at the same time.",
+
+      "I watched and I like all of the movies and TV shows that are shown on this website, so people will be able to watch it if they need a recommendation for movie night ",
     ],
     mainProgram: "Laravel 8",
     dependencies: "Laravel-mix, Bootstrap, Axios, Sass",
@@ -36,8 +40,10 @@ export const projects: ProjectType[] = [
     slug: "rs-uripsumoharjo",
     description: [
       "A website design proposal for a local hospital in Bandar Lampung. The design wasn't approved by my client but I can keep it for my own portfolio",
+
       "Inspired by Mayo Clinic's website. A very simple design, easy-to-use, informative but not too crowded with too many useless details.",
-      "The core of this website design is Next.JS, supported by Redux Toolkit, Formik, Yup and React leaflet",
+
+      "The core of this website design is Next.JS, supported by Redux Toolkit, Formik, Yup, and React leaflet",
     ],
     mainProgram: "Next.js",
     dependencies: "Typescript, Tailwind CSS, Redux Toolkit, Formik/Yup",
@@ -50,7 +56,9 @@ export const projects: ProjectType[] = [
     slug: "azriclone",
     description: [
       "This website is based on Facebook's website design, mainly to showcase my ability in accessing and processing data with Rest API.",
-      "All mockup data in this website are from DUMMYAPI.IO, a free ready to use API Service filled with dummy data",
+
+      "All mock-up data in this website are from DUMMYAPI.IO, a free ready-to-use API Service filled with dummy data",
+
       "Combining Typescript with Redux/toolkit, Redux/persist and Next Redux Wrapper, was a bit of a challenge for me, but turned out to be very useful for later use",
     ],
     mainProgram: "Next.js",
@@ -61,7 +69,7 @@ export const projects: ProjectType[] = [
 export const aboutData = {
   message: [
     "Hello, I'm Riza Hariati (依扎 / Icha).",
-    "Welcome to IchaCodes portfolio website showcasing a selected few of the websites I have built since 2020.",
+    "Welcome to the IchaCodes portfolio website showcasing a selected few of the websites I have built since 2020.",
     "I mostly work on Front End Developing, from design to testing to deployment, and I also dabble on the Back End creating simple CRUD data. My favorite programming tool is React.JS, but I also enjoy working with Laravel and created a few websites using it.",
     "I have also used Java to help me learn algorithms and data structure, which was so much fun for me.",
     "Thank you for visiting!",
